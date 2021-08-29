c64c0aaa193c56e8cfe375edba3bbb2deb8084b2
Jumbo Daniel <jumbodaniel9@gmail.com>
2021-08-29 20:27:28 +0800

    Minor edit

--- src/components/Gamer/gamer.js
+++ src/components/Gamer/gamer.js
@@ -47,9 +47,9 @@ const Gamer = () => {
           </div>
         </div>
       </div>
-      <div>
+      {/* <div>
         <h2>Some of my favourite gaming titles</h2>
-      </div>
+      </div> */}
       <Footer/>
       </div>
     );
